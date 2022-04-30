fc475c0c5106af8878349de52accf44aa39e00c1
Yichen Yao <yy486@cornell.edu>
2022-04-30 19:57:26 -0400

    init db

--- src/db.py
+++ src/db.py
@@ -2,118 +2,79 @@ from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
 
-# association tables for instructors, students, and assignments
-assoc_ins_table = db.Table(
-    "ins_table",
+# association tables for foods and tags
+assoc_table = db.Table(
+    "association",
     db.Model.metadata,
-    db.Column("course_id", db.Integer, db.ForeignKey("course.id")),
-    db.Column("user_id", db.Integer, db.ForeignKey("user.id"))
-)
-
-assoc_stu_table = db.Table(
-    "stu_table",
-    db.Model.metadata,
-    db.Column("course_id", db.Integer, db.ForeignKey("course.id")),
-    db.Column("user_id", db.Integer, db.ForeignKey("user.id"))
+    db.Column("food_id", db.Integer, db.ForeignKey("tag.id")),
+    db.Column("tag_id", db.Integer, db.ForeignKey("food.id"))
 )
 
 
-class Course(db.Model):
-    __tablename__ = "course"
+class Food(db.Model):
+    __tablename__ = "food"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    code = db.Column(db.String, nullable=False)
     name = db.Column(db.String, nullable=False)
-    instructors = db.relationship(
-        "User", secondary=assoc_ins_table, back_populates="ins_courses")
-    students = db.relationship(
-        "User", secondary=assoc_stu_table, back_populates="stu_courses")
-    assignments = db.relationship("Assignment", cascade="delete")
+    description = db.Column(db.String, nullable=False)
+    calorie = db.Column(db.Integer, nullable=False)
+    image = db.Column(db.String, nullable=False)
+    # to be debugged
+    tags = db.relationship(
+        "Tag", secondary=assoc_table, back_populates="tags")
 
     def __init__(self, **kwargs):
-        # initialize course object/entry
-        self.code = kwargs.get("code", "")
+        # initialize food object/entry
         self.name = kwargs.get("name", "")
+        self.description = kwargs.get("name", "")
+        self.image = kwargs.get("name", "")
+        self.calorie = kwargs.get("name",)
 
     def serialize(self):
         return {
             "id": self.id,
-            "code": self.code,
             "name": self.name,
-            "assignments": [a.simple_serialize() for a in self.assignments],
-            "instructors": [i.simple_serialize() for i in self.instructors],
-            "students": [s.simple_serialize() for s in self.students]
+            "description": self.code,
+            "calorie": self.calorie,
+            "image": self.image,
+            "tags": [t.simple_serialize() for t in self.tags],
         }
 
     def simple_serialize(self):
         return {
             "id": self.id,
-            "code": self.code,
-            "name": self.name
+            "name": self.name,
+            "description": self.code,
+            "calorie": self.calorie,
+            "image": self.image
         }
 
 
-class User(db.Model):
-    __tablename__ = "user"
+class Tag(db.Model):
+    __tablename__ = "tags"
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String, nullable=False)
-    netid = db.Column(db.String, nullable=False)
-    ins_courses = db.relationship(
-        "Course", secondary=assoc_ins_table, back_populates="instructors")
-    stu_courses = db.relationship(
-        "Course", secondary=assoc_stu_table, back_populates="students")
+
+    # Stored as a hex string
+    color = db.Column(db.String, nullable=False)
+    foods = db.relationship(
+        "food", secondary=assoc_table, back_populates="tags")
 
     def __init__(self, **kwargs):
         # initialize user object/entry
         self.name = kwargs.get("name", "")
-        self.netid = kwargs.get("netid", "")
+        self.color = kwargs.get("color", "")
 
     def serialize(self):
-        course = []
-        for c in self.ins_courses:
-            course.append(c.simple_serialize())
-        for c in self.stu_courses:
-            course.append(c.simple_serialize())
         return {
             "id": self.id,
             "name": self.name,
-            "netid": self.netid,
-            "courses": course
+            "color": self.color,
+            "foods": [f.simple_serialize() for f in self.foods],
         }
 
     def simple_serialize(self):
         return {
             "id": self.id,
             "name": self.name,
-            "netid": self.netid
-        }
-
-
-class Assignment(db.Model):
-    __tablename__ = "assignment"
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String, nullable=False)
-    due_date = db.Column(db.Integer, nullable=False)
-    course_id = db.Column(db.Integer, db.ForeignKey(
-        "course.id"), nullable=False)
-
-    def __init__(self, **kwargs):
-        # initialize assignment object/entry
-        self.title = kwargs.get("title", "")
-        self.due_date = kwargs.get("due_date")
-        self.course_id = kwargs.get("course_id")
-
-    def serialize(self):
-        course = Course.query.filter_by(id=self.course_id).first()
-        return {
-            "id": self.id,
-            "title": self.title,
-            "due_date": self.due_date,
-            "course": course.simple_serialize()
-        }
-
-    def simple_serialize(self):
-        return {
-            "id": self.id,
-            "title": self.title,
-            "due_date": self.due_date,
+            "color": self.color,
         }
